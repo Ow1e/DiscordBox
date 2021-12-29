44a1f4eb5854611e91ae8528bd337a5c536e9acf
Owen Shaule <55895503+Ow1e@users.noreply.github.com>
2021-12-29 18:44:55 -0500

    Drive fix

--- driver.py
+++ driver.py
@@ -2,15 +2,16 @@
 import properties
 
 if not properties.emulate:
-    from Adafruit_Thermal import *
-
-    printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
-    printer.println("==== DiscordBox ====")
-
-
+    import serial, adafruit_thermal_printer
+    uart = serial.Serial("/dev/serial0", baudrate=19200, timeout=3000)
+    ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.69)
+    printer = ThermalPrinter(uart)
+    printer.feed(2)
+    printer.print("Initializing support... ")
     def send(msg):
-        printer.println(msg)
         print(msg)
+        printer.print(msg)
+    printer.print("Done!")
 else:
     def send(msg):
         print(msg)
